6018739c5c44ab2ad5e4cb4eb11c9cdd25a8dce5
joelmorland <joelmorland@gmail.com>
2012-01-12 15:54:07 +1100

    Updated title

--- app/Controller/GalleryController.php
+++ app/Controller/GalleryController.php
@@ -17,7 +17,7 @@ class GalleryController extends AppController
         $search=(isset($this->params['url']['query']))?$this->params['url']['query']:'sunset';
         
         #set page title
-        $this->set('title_for_layout', "Flickrourice '$search' | P$page");
+        $this->set('title_for_layout', "Flickrish '$search' | P$page");
                
         #execute search function
         $photos = $flickr->searchPhotos($search, $page);        
